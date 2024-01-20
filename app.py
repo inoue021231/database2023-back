345f37ab59f982a11574d79e22eb349298696eb8
inoue_r <chru21072@g.nihon-u.ac.jp>
2024-01-20 10:38:20 +0900

    ユーザー機能を追加

--- app.py
+++ app.py
@@ -8,14 +8,14 @@ cors = CORS(app)
 
 connection = psycopg.connect(
     host='localhost',
-    dbname='tododata',
+    dbname='taskdata',
     user='postgres',
     password='password',
 )
 
 
 @app.route('/tododata', methods=['GET'])
-def get_publishers():
+def get_task():
     sql = '''
     SELECT *
     FROM タスク;
@@ -28,10 +28,10 @@ def get_publishers():
 
 
 @app.route('/tododata/create', methods=['POST'])
-def post_publisher():
+def post_task():
 
     sql = '''
-    SELECT ID
+    SELECT タスクID
     FROM タスク;
     '''
     result = connection.execute(sql)
@@ -47,11 +47,23 @@ def post_publisher():
     
     try:
         sql = '''
-        INSERT INTO タスク (ID, タスク内容, 日時, ステータス)
+        INSERT INTO タスク (タスクID, タスク内容, 日時, ステータス)
         VALUES
         (%(todo_id)s, %(todo_text)s, %(todo_date)s, %(todo_status)s);
         '''
         connection.execute(sql,{'todo_id': new_key, 'todo_text': content["text"], 'todo_date': content["date"], 'todo_status': content["status"]})
+    except Exception:
+        connection.rollback()
+    else:
+        connection.commit()
+    
+    try:
+        sql = '''
+        INSERT INTO タスクデータ (タスクID, ユーザーID)
+        VALUES
+        (%(task_id)s,%(user_id)s);
+        '''
+        connection.execute(sql,{'task_id': new_key, 'user_id': content["userID"]})
     except Exception:
         connection.rollback()
     else:
@@ -61,12 +73,12 @@ def post_publisher():
     return jsonify({'message': 'created'})
 
 @app.route('/tododata/update', methods=['POST'])
-def update_tododata():
+def update_task():
     content = request.get_json()
     print(content)
     try:
         sql = '''
-        UPDATE タスク SET ステータス = %(todo_status)s WHERE ID = %(todo_id)s;
+        UPDATE タスク SET ステータス = %(todo_status)s WHERE タスクID = %(todo_id)s;
         '''
         connection.execute(sql,{'todo_status': content["status"], 'todo_id': content["id"]})
     except Exception:
@@ -77,10 +89,10 @@ def update_tododata():
     return jsonify({'message': 'updated'})
 
 @app.route('/tododata/delete/<int:id>', methods=['POST'])
-def delete_tododata(id):
+def delete_task(id):
     try:
         sql = '''
-        DELETE FROM タスク WHERE ID = %(todo_id)s
+        DELETE FROM タスクデータ WHERE タスクID = %(todo_id)s
         '''
         connection.execute(sql,{'todo_id': id})
     except Exception:
@@ -89,3 +101,48 @@ def delete_tododata(id):
         connection.commit()
     
     return jsonify({'message': 'created'})
+
+@app.route('/user', methods=['GET'])
+def get_user():
+    sql = '''
+    SELECT *
+    FROM ユーザー;
+    '''
+    result = connection.execute(sql)
+    user = []
+    for row in result:
+        user.append({'userID': row[0], 'userName': row[1], 'userPassword':row[2]})
+    return jsonify(user)
+
+@app.route('/user/create', methods=['POST'])
+def post_user():
+    content = request.get_json()
+    
+    try:
+        sql = '''
+        INSERT INTO ユーザー (ユーザーID, 名前, パスワード)
+        VALUES
+        (%(user_id)s, %(user_name)s, %(user_password)s);
+        '''
+        connection.execute(sql,{'user_id': content["userID"], 'user_name': content["userName"], 'user_password': content["userPassword"]})
+    except Exception:
+        connection.rollback()
+    else:
+        connection.commit()
+
+    
+    return jsonify({'message': 'created'})
+
+@app.route('/taskdata',methods=['POST'])
+def get_taskdata():
+    content = request.get_json()
+    sql = '''
+    SELECT タスク.タスクID, タスク内容, 日時, ステータス 
+    FROM タスク JOIN タスクデータ ON タスク.タスクID = タスクデータ.タスクID 
+    WHERE ユーザーID = %(user_id)s;
+    '''
+    result = connection.execute(sql,{'user_id': content["userID"]})
+    publishers = []
+    for row in result:
+        publishers.append({'id': row[0], 'text': row[1], 'date':row[2], 'status':row[3]})
+    return jsonify(publishers)
--- tododata.sql
+++ tododata.sql
@@ -1,9 +1,26 @@
-CREATE DATABASE tododata;
+CREATE DATABASE taskdata;
 
 DROP TABLE IF EXISTS タスク;
 CREATE TABLE タスク (
-    ID INTEGER PRIMARY KEY,
+    タスクID INTEGER PRIMARY KEY,
     タスク内容 TEXT NOT NULL,
     日時 DATE NOT NULL,
     ステータス TEXT NOT NULL
 );
+
+DROP TABLE IF EXISTS ユーザー;
+CREATE TABLE ユーザー (
+    ユーザーID TEXT PRIMARY KEY,
+    名前 TEXT NOT NULL,
+    パスワード TEXT NOT NULL
+);
+
+DROP TABLE IF EXISTS タスクデータ;
+CREATE TABLE タスクデータ (
+    タスクID INTEGER REFERENCES タスク(タスクID),
+    ユーザーID TEXT REFERENCES ユーザー(ユーザーID)
+);
+
+INSERT INTO ユーザー (ユーザーID,名前,パスワード)
+VALUES
+    ('bunri01', '文理太郎', '20240120');
